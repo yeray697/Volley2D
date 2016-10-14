e9c91d9efdb39a150973bd2043e183c6365b93c5
yeray697 <yeray.1997.yr@gmail.com>
2016-10-14 05:32:09 +0100

    Project added
    Playes' movement, ball and field done

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Ball : MonoBehaviour {
+
+    float minForce = 20;
+    float maxForce = 55;
+    public float timer = 3;
+    public GameObject textObject;
+    public TextMesh text;
+    public bool started = false;
+    public bool timerDone = false;
+    // Use this for initialization
+    void Start()
+    {
+        text = textObject.GetComponent<TextMesh>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (started && !timerDone)
+        {
+            Vector2 vectorAux;
+            float forceAux;
+            forceAux = Random.Range(minForce, maxForce);
+            if (Random.Range(0, 2) == 0)
+                vectorAux = Vector2.left * forceAux;
+            else
+                vectorAux = Vector2.right * forceAux;
+            GetComponent<Rigidbody2D>().AddForce(vectorAux * forceAux);
+            timerDone = true;
+        }
+        else if (!timerDone && !started)
+        {
+            timer -= Time.deltaTime;
+            if (timer >= 2 && timer < 3)
+                text.text = "2";                    
+            else if (timer >= 1 && timer < 2)
+                text.text = "1";
+            else if (timer >= 0 && timer < 1)
+                text.text = "GO";
+            else if( timer < -0.5)
+            {
+                text.text = "";
+                started = true;
+                GetComponent<Rigidbody2D>().isKinematic = false;
+            }
+        }
+	}
+}
---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,80 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour {
+
+    public GameObject player1;
+    public GameObject player2;
+
+    Rigidbody2D player1RB;
+    Rigidbody2D player2RB;
+    public bool isGrounded1 = true;
+    public bool isGrounded2 = true;
+
+    public float jumpSpeed = -20F;
+    public float movementSpeed = 20F;
+
+	// Use this for initialization
+	void Start () {
+
+        player1RB = player1.GetComponent<Rigidbody2D>();
+        player2RB = player2.GetComponent<Rigidbody2D>();
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+ 
+    }
+
+    void FixedUpdate()
+    {
+        float horizontal1 = Input.GetAxis("Horizontal1");
+        float horizontal2 = Input.GetAxis("Horizontal2");
+        float vertical1 = Input.GetAxis("Vertical1");
+        float vertical2 = Input.GetAxis("Vertical2");
+
+        //Can jump when is over the ground
+        //isGrounded1 = player1.transform.position.y <= 0.64F;
+        //isGrounded2 = player2.transform.position.y <= 0.64F;
+
+        //Can jump when Y velocity is 0
+        isGrounded1 = player1RB.velocity.y == 0;
+        isGrounded2 = player2RB.velocity.y == 0;
+
+        //Player 1
+        //Horizontal movement
+        if (horizontal1 != 0)
+        {
+            if (horizontal1 > 0)
+                player1.transform.eulerAngles = new Vector2(0, 0);
+            if (horizontal1 < 0)
+                player1.transform.eulerAngles = new Vector2(0, 180);
+            player1.transform.Translate(Vector2.right * movementSpeed * Time.deltaTime);
+        }
+        //Vertical movement
+        if (vertical1 > 0 && isGrounded1)
+        {
+            player1RB.AddForce(Vector2.up * jumpSpeed, ForceMode2D.Impulse);
+            isGrounded1 = false;
+        }
+
+        //Player 2
+        //Horizontal movement
+        if (horizontal2 != 0)
+        {
+            if (horizontal2 > 0)
+                player2.transform.eulerAngles = new Vector2(0, 0);
+            if (horizontal2 < 0)
+                player2.transform.eulerAngles = new Vector2(0, 180);
+            player2.transform.Translate(Vector2.right * movementSpeed * Time.deltaTime);
+        }
+        //Vertical movement
+        if (vertical2 > 0 && isGrounded2)
+        {
+            player2RB.AddForce(Vector2.up * jumpSpeed, ForceMode2D.Impulse);
+            isGrounded2 = false;
+        }
+
+    }
+}
