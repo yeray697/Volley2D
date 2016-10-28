35f83e1f944794619fbf17370a3a7f12e3ee5faf
Yeray Ruiz <yeray.1997.yr@gmail.com>
2016-10-28 12:00:44 +0200

    Finished! (Apparently)

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -4,6 +4,46 @@ using UnityEngine.UI;
 
 public class Ball : MonoBehaviour {
 
+<<<<<<< HEAD
+	//Objetos
+	public GameObject topWall;					//Muro con un collider para detectar cuando está la bola fuera de pantalla
+	public Text contador;						//Contador
+	public Text textAux;						//Mensajes auxiliares
+	public Text puntosJugador1;					//Puntos del jugador 1
+	public Text puntosJugador2;					//Puntos del jugador 2
+
+	public GameObject ballOutOfScreenGoingTop;	//Prefab que aparecerá cuando la bola esté fuera de pantalla
+	GameObject ballOutOfScreenActive;			//Objeto clonado del prefab que se inicializa 
+												//al entrar al collider y se destruye al salir
+	public GameObject ballOutOfScreenGoingBot;	//Prefab que aparecerá cuando la bola esté fuera de pantalla
+	Sprite ballOutOfScreenGoingBotSprite; 		//Render del objeto anterior, que cambiará el sprite cuando la dirección
+												//  de la bola sea negativa (hacia abajo)
+	AudioSource jumpSound;						//Sonido cuando salta
+	//Variables
+	public int winner = -1;						//Indica quién ha ganado
+												//	-1 = Aún no ha empezado
+												//	0 = La partida está en juego
+												//	1 = Ha ganado el jugador 1
+												//	2 = Ha ganado el jugador 2
+    float minForce = 9F;						//Fuerza mínima para lanzar la bola al inicio
+    float maxForce = 13.51F;						//Fuerza máxima para lanzar la bola al inicio
+    float timer = 3;							//Tiempo inicial del contador
+    bool started = false;						//Booleano para saber si la partida ha empezado
+    bool timerDone = false;						//Booleano para saber si el contador ha finalizado
+	Vector3 posicionInicial;					//Posición inicial de la bola
+	int puntos1;								//Puntos del jugador 1
+	int puntos2;								//Puntos del jugador 2
+    // Use this for initialization
+    void Start()
+	{
+		posicionInicial = this.transform.position;
+		puntos1 = 0;
+		puntos2 = 0;
+		ballOutOfScreenGoingBotSprite = ballOutOfScreenGoingBot.GetComponent<SpriteRenderer> ().sprite;
+		jumpSound = this.GetComponent<AudioSource> ();
+		contador.gameObject.SetActive (false);
+		textAux.gameObject.SetActive (true);
+=======
     float minForce = 20;
     float maxForce = 55;
     public float timer = 3;
@@ -15,19 +55,114 @@ public class Ball : MonoBehaviour {
     void Start()
     {
         text = textObject.GetComponent<TextMesh>();
+>>>>>>> e9c91d9efdb39a150973bd2043e183c6365b93c5
     }
 	
 	// Update is called once per frame
 	void Update () {
         if (started && !timerDone)
         {
+<<<<<<< HEAD
+			//Impulsa inicialmente la bola hacia un lado aleatorio y con una fuerza aleatoria
+            Vector2 vectorAux;
+            float forceAux;
+				//Obteniendo fuerza aleatoria
+            forceAux = Random.Range(minForce, maxForce);
+				//Obteniendo lado aleatorio
+=======
             Vector2 vectorAux;
             float forceAux;
             forceAux = Random.Range(minForce, maxForce);
+>>>>>>> e9c91d9efdb39a150973bd2043e183c6365b93c5
             if (Random.Range(0, 2) == 0)
                 vectorAux = Vector2.left * forceAux;
             else
                 vectorAux = Vector2.right * forceAux;
+<<<<<<< HEAD
+			//Impulsando bola
+            GetComponent<Rigidbody2D>().AddForce(vectorAux * forceAux);
+            timerDone = true;
+        }
+		else if (!timerDone && !started && winner == 0)
+        {
+            timer -= Time.deltaTime;
+            if (timer >= 2 && timer < 3)
+				contador.text = "2";                    
+            else if (timer >= 1 && timer < 2)
+				contador.text = "1";
+            else if (timer >= 0 && timer < 1)
+				contador.text = "GO";
+            else if( timer < -0.3)
+            {
+				contador.text = "";
+                started = true;
+				//Ponemos la bola como kinematic para que le afecten las fuerzas,
+				//  ya que durante el timer se encontraba "pausada"
+                GetComponent<Rigidbody2D>().isKinematic = false;
+				contador.gameObject.SetActive (false);
+            }
+        }
+		if (winner != 0) {
+			//Si se pulsa el Espacio, reiniciamos la partida
+			if (Input.GetAxis("Restart") != 0) {
+				this.transform.position = posicionInicial;
+				winner = 0;
+				started = false;
+				timerDone = false;
+				timer = 3.7F;
+				contador.text = "3";
+				contador.gameObject.SetActive (true);
+				textAux.gameObject.SetActive (false);
+			}
+		}
+
+		if (ballOutOfScreenActive != null) {
+			if (this.GetComponent<Rigidbody2D>().velocity.y < 0) { 
+				//Si está cayendo, cambiamos el sprite
+				ballOutOfScreenActive.GetComponent<SpriteRenderer>().sprite = ballOutOfScreenGoingBotSprite;				
+			}
+		}
+	}
+	void OnTriggerEnter2D (Collider2D other)
+	{
+		//Si contacta con el scanner (la bola está fuera de la pantalla), inicializa un prefab para indicar por dónde va la bola
+		if (other.name == "TopWallScanner") {
+			ballOutOfScreenActive = (GameObject) Instantiate (ballOutOfScreenGoingTop, new Vector3 (this.transform.position.x, 4.2F, 0), new Quaternion (0, 0, 0, 0));
+		}
+	}
+	void OnTriggerExit2D(Collider2D other){
+		//Si sale del scanner (la bola se ve en la pantalla), destruye el prefab indicando por donde va la bola
+		if (other.name == "TopWallScanner") {
+			Destroy (ballOutOfScreenActive);
+		}
+	}
+	void OnTriggerStay2D(Collider2D other) {
+		//Mientras está la bola en el scanner (fuera de la pantalla), actualiza su posición respecto al eje X de la bola
+		if (other.name == "TopWallScanner") {
+			ballOutOfScreenActive.transform.position = new Vector3(this.transform.position.x, ballOutOfScreenActive.transform.position.y,0);
+		}
+	}
+	void OnCollisionEnter2D(Collision2D coll){
+		//Si la bola contacta con el suelo, comprobamos quién ha ganado
+		if (coll.gameObject.name == "Ground") {
+			if (this.transform.position.x < 0) {
+				winner = 2;
+				puntos2++;
+				puntosJugador2.text = puntos2.ToString ();
+			} else {
+				winner = 1;
+				puntos1++;
+				puntosJugador1.text = puntos1.ToString ();
+			}
+			textAux.gameObject.SetActive (true);
+			textAux.text = "Punto para el jugador "+winner;
+			//Bloqueamos la bola
+			this.GetComponent<Rigidbody2D>().isKinematic = true;
+		}
+		if (coll.gameObject.name.StartsWith("Player")) {
+			jumpSound.Play ();
+		}
+=======
             GetComponent<Rigidbody2D>().AddForce(vectorAux * forceAux);
             timerDone = true;
         }
@@ -47,5 +182,6 @@ public class Ball : MonoBehaviour {
                 GetComponent<Rigidbody2D>().isKinematic = false;
             }
         }
+>>>>>>> e9c91d9efdb39a150973bd2043e183c6365b93c5
 	}
 }
--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -3,6 +3,27 @@ using System.Collections;
 
 public class Movement : MonoBehaviour {
 
+<<<<<<< HEAD
+	//Objetos
+    public GameObject player1;				//Jugador 1
+    public GameObject player2;				//Jugador 2
+	Ball ballScript;						//Script
+	public GameObject ballWithTheScript;	//Objeto que posee el script anterior
+    Rigidbody2D player1RB;					//RigidBody del jugador 1
+    Rigidbody2D player2RB;					//RigidBody del jugador 2
+
+	//Variables
+	bool isGrounded1 = true;			//Indica si si eje Y está quieto, para poder saltar
+	bool isGrounded2 = true;			//Indica si si eje Y está quieto, para poder saltar
+	float jumpSpeed = 20F;					//Velocidad de salto (fuerza)
+	float movementSpeed = 5F;				//Velocidad de movimiento
+
+	// Use this for initialization
+	void Start () {
+        player1RB = player1.GetComponent<Rigidbody2D>();
+        player2RB = player2.GetComponent<Rigidbody2D>();
+		ballScript = ballWithTheScript.GetComponent<Ball> ();
+=======
     public GameObject player1;
     public GameObject player2;
 
@@ -25,10 +46,70 @@ public class Movement : MonoBehaviour {
 	void Update ()
     {
  
+>>>>>>> e9c91d9efdb39a150973bd2043e183c6365b93c5
     }
 
     void FixedUpdate()
     {
+<<<<<<< HEAD
+		//Si es 0, es que la partida está en curso
+		if (ballScript.winner == 0) {
+			player1RB.isKinematic = false;
+			player2RB.isKinematic = false;
+
+			float horizontal1 = Input.GetAxis ("Horizontal1");
+			float horizontal2 = Input.GetAxis ("Horizontal2");
+			float vertical1 = Input.GetAxis ("Vertical1");
+			float vertical2 = Input.GetAxis ("Vertical2");
+
+			//Puede saltar cuando su velocidad en Y es 0
+			isGrounded1 = player1RB.position.y <= -2.22F;
+			isGrounded2 = player2RB.position.y <= -2.22F;
+
+			//Jugador 1
+			//Movimiento horizontal
+			if (horizontal1 != 0) {
+				if (horizontal1 > 0) {
+					player1.transform.eulerAngles = new Vector2 (0, 0);
+					//aux = 
+				}
+				if (horizontal1 < 0) {
+					player1.transform.eulerAngles = new Vector2 (0, 180);
+				}
+
+				player1RB.velocity = new Vector2 (horizontal1 * movementSpeed, player1RB.velocity.y);
+				//player1.transform.Translate(Vector2.right * movementSpeed * Time.deltaTime);
+			}
+			//Movimiento vertical
+			if (vertical1 > 0 && isGrounded1) {
+				player1RB.AddForce (Vector2.up * jumpSpeed, ForceMode2D.Impulse);
+				isGrounded1 = false;
+			}
+
+			//Jugador 2
+			//Movimiento horizontal
+			if (horizontal2 != 0) {
+				if (horizontal2 > 0) {
+					player2.transform.eulerAngles = new Vector2 (0, 180);
+				}
+				if (horizontal2 < 0) {
+					player2.transform.eulerAngles = new Vector2 (0, 0);
+				}
+
+				player2RB.velocity = new Vector2 (horizontal2 * movementSpeed, player2RB.velocity.y);
+				//player2.transform.Translate(Vector2.left * movementSpeed * Time.deltaTime);
+			}
+			//Movimiento vertical
+			if (vertical2 > 0 && isGrounded2) {
+				player2RB.AddForce (Vector2.up * jumpSpeed, ForceMode2D.Impulse);
+				isGrounded2 = false;
+			}
+		} else {
+			//Si la partida finaliza, bloqueamos los jugadores y llamamos la escena del menú
+			player1RB.isKinematic = true;
+			player2RB.isKinematic = true;
+		}
+=======
         float horizontal1 = Input.GetAxis("Horizontal1");
         float horizontal2 = Input.GetAxis("Horizontal2");
         float vertical1 = Input.GetAxis("Vertical1");
@@ -76,5 +157,6 @@ public class Movement : MonoBehaviour {
             isGrounded2 = false;
         }
 
+>>>>>>> e9c91d9efdb39a150973bd2043e183c6365b93c5
     }
 }
